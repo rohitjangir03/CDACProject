7a4e3ca8dcc7991e4c128e0ffa66ddedc80cceeb
Rohit Kumar <jangirrohitrj03@gmail.com>
2023-08-30 09:45:36 +0530

    initial

--- SparshCode/src/main/java/com/app/DTO/AppConstants.java
+++
@@ -1,8 +0,0 @@
-package com.app.DTO;
-
-public class AppConstants {
-   public static final String PAGE_NUMBER_STRING="0";
-   public static final String PAGE_SIZE_STRING="2";
-   public static final String SORT_BY_STRING="plantId";
-   public static final String SORT_DIR_STRING="asc";
-}
--- SparshCode/src/main/java/com/app/Model/Cart.java
+++
@@ -1,5 +0,0 @@
-package com.app.Model;
-
-public class Cart {
-
-}
